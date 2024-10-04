2644576ac0a085d78c851b87a62ad415194acc33
Mrhehan <muhammadrhehan150@gmail.com>
2024-10-04 16:11:11 +0700

    project super admin

--- app/(authenticated)/super-admin/[idUser]/project/page.tsx
+++ app/(authenticated)/super-admin/[idUser]/project/page.tsx
@@ -1,96 +1,62 @@
 'use client';
 
 import React, { useState } from 'react';
-import { Divider, Radio, Table, Button } from 'antd';
+import { Divider, Radio, Table, Button, Tabs, TabsProps } from 'antd';
 import type { TableColumnsType, TableProps } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
+import { useParams } from 'next/navigation';
 
-// interface DataType {
-//   key: React.Key;
-//   name: string;
-//   age: number;
-//   address: string;
-// }
 
-// const columns: TableColumnsType<DataType> = [
-//   {
-//     title: 'Name',
-//     dataIndex: 'name',
-//     render: (text: string) => <a>{text}</a>,
-//   },
-//   {
-//     title: 'Age',
-//     dataIndex: 'age',
-//   },
-//   {
-//     title: 'Address',
-//     dataIndex: 'address',
-//   },
-// ];
+const Page: React.FC = () => {
 
-// const data: DataType[] = [
-//   {
-//     key: '1',
-//     name: 'John Brown',
-//     age: 32,
-//     address: 'New York No. 1 Lake Park',
-//   },
-//   {
-//     key: '2',
-//     name: 'Jim Green',
-//     age: 42,
-//     address: 'London No. 1 Lake Park',
-//   },
-//   {
-//     key: '3',
-//     name: 'Joe Black',
-//     age: 32,
-//     address: 'Sydney No. 1 Lake Park',
-//   },
-//   {
-//     key: '4',
-//     name: 'Disabled User',
-//     age: 99,
-//     address: 'Sydney No. 1 Lake Park',
-//   },
-// ];
+    const [activeKey, setActiveKey] = useState<string>('pending');
+    const params = useParams();
 
-// // rowSelection object indicates the need for row selection
-// const rowSelection: TableProps<DataType>['rowSelection'] = {
-//   onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
-//     console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-//   },
-//   getCheckboxProps: (record: DataType) => ({
-//     disabled: record.name === 'Disabled User', // Column configuration not to be checked
-//     name: record.name,
-//   }),
-// };
+    const onChange: TabsProps['onChange'] = (key) => {
+        setActiveKey(key);
+    };
 
-const App: React.FC = () => {
-  const [selectionType, setSelectionType] = useState<'checkbox' | 'radio'>('checkbox');
+    const items: TabsProps['items'] = [
+        {
+            key: 'pending',
+            label: 'Pending',
+        },
+        {
+            key: 'on-progres',
+            label: 'On Progres',
+        },
+        {
+            key: 'done',
+            label: 'Done',
+        },
+        {
+            key: 'approved',
+            label: 'Approved'
+        },
+    ];
 
-  return (
-    <div>
-        <h1 style={{ fontSize: 30, padding: 20}} >Project
-            <Button type='primary' style={{ marginLeft: 823 }}>
-                <PlusOutlined />Tambah
-            </Button>
-        </h1>
-        <div style={{ padding: 24, minHeight: '100vh', backgroundColor: '#FFFFFF', borderRadius: 15 }}>
-            {/* <Radio.Group onChange={(e) => setSelectionType(e.target.value)} value={selectionType}>
-                <Radio value="checkbox">Checkbox</Radio>
-                <Radio value="radio">radio</Radio>
-            </Radio.Group>
-            <Divider />
-            <Table<DataType>
-                rowSelection={{ type: selectionType, ...rowSelection }}
-                columns={columns}
-                dataSource={data}
-            /> */}
+    return (
+        <div>
+            <h1 style={{ fontSize: 30, padding: 20}} >Project
+                <Button type='primary' style={{ marginLeft: 880 }}>
+                    <PlusOutlined />Tambah
+                </Button>
+            </h1>
+            <div style={{ 
+                    paddingLeft: 24, 
+                    paddingRight: 24, 
+                    paddingBottom: 24, 
+                    minHeight: '100vh', 
+                    backgroundColor: '#FFFFFF', 
+                    borderRadius: 15 
+                    }}>
+                <div>
+                    <Tabs defaultActiveKey='pending' items={items} onChange={onChange}/>
+                </div>
+            </div>
+        
         </div>
-      
-    </div>
-  );
+    );
 };
 
-export default App;
+export default Page;
